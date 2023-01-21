f8a3fd691a81dd44eb9b3c4dfada7b9bf986471d
ElDuko <penevl@protonmail.com>
2023-01-21 15:07:56 +0200

    Scripts now get executed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ console.log("Starting webhook executor...");
 
 const fs = require("fs");
 const express = require('express')
+const { exec } = require('child_process');
 const app = express()
 
 let hooksJson = JSON.parse(fs.readFileSync("./hooks/hooks.json"));
@@ -16,8 +17,22 @@ hooksJson.forEach(element => {
     console.log("Created a webhook listener for \"" + name + "\" with the url of \"" + url  +"\"")
 
     app.post(element.url, (req, res) => {
+
         console.log("\"" + name + "\" Has been hit with a request")
+        console.log("Executing script \"" + script + "\"")
+
+        exec("sh " + __dirname + "/../scripts/" + script, (error, stdout, stderr) => {
+            console.log("[SCRIPT EXECUTION START]")
+            console.log(stdout);
+            console.error(stderr);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+            console.log("[SCRIPT EXECUTION END]")
+        });
+
         res.status(200).end()
+
       })
 
 });
