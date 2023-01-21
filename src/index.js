3f85a0e1accd8fa03bd4736e4cf12bcd463f14ba
elduko <elduko@pop-os.localdomain>
2023-01-21 13:38:26 +0200

    Added reading the hooks.json file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log("Starting webhook executor...")
+
+const fs = require("fs")
+
+var hooksRawJson;
+
+fs.readFile("./hooks/hooks.json", 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    hooksRawJson = data;
+});
